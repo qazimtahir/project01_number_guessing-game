2002470535f3ee99a86f62cfd630723ba4b8c300
Muhammad Tahir Iqbal <qazimtahir@gmail.com>
2024-01-11 02:46:59 +0500

    Number Guessing Game

---
+++ index.ts
@@ -0,0 +1,24 @@
+// Number Guessing Game
+
+import inquirer from "inquirer";
+
+let systemGeneratedNumber = Math.floor(Math.random() * 10);
+
+let answers = await inquirer.prompt([
+  {
+    type: "input",
+    name: "guess",
+    message: "Guess a number between 0 and 10",
+  },
+]);
+
+let guess = parseInt(answers.guess);
+
+if (guess === systemGeneratedNumber) {
+  console.log("You Win!");
+  process.exit();
+} else {
+  console.log("You Lose!");
+  console.log(`System Number is ${systemGeneratedNumber}.`);
+  process.exit();
+}
